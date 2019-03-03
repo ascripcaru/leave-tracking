2d1cd41c72e8fe7581a703da21f8112e02703648
andrei scripcaru <andrei.scripcaru@issco.ro>
2019-03-03 16:27:25 +0200

    use native timestamps and fix lint errors

--- backend/src/server/auth-middleware.js
+++ backend/src/server/auth-middleware.js
@@ -1,26 +1,26 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const http = axios.create({ baseURL: process.env.AUTH_URL })
+const http = axios.create({ baseURL: process.env.AUTH_URL });
 
 async function withAuth(req, res, next) {
     try {
-        const { data } = await http.get('/verify', { headers: { authorization: req.headers.authorization } })
-        req.token = data
-        next()
+        const { data } = await http.get('/verify', { headers: { authorization: req.headers.authorization } });
+        req.token = data;
+        next();
     } catch (error) {
-        const { status, data } = error.response
-        res.status(status).json(data)
+        const { status, data } = error.response;
+        res.status(status).json(data);
     }
 }
 
 async function getMe(req, res, next) {
     try {
-        const me = await http.get('/me', { headers: { authorization: req.headers.authorization } })
-        res.json(me.data)
+        const me = await http.get('/me', { headers: { authorization: req.headers.authorization } });
+        res.json(me.data);
     } catch (error) {
-        const { status, data } = error.response
-        res.status(status).json(data)
+        const { status, data } = error.response;
+        res.status(status).json(data);
     }
 }
 
-export { withAuth, getMe }
+export { withAuth, getMe };
